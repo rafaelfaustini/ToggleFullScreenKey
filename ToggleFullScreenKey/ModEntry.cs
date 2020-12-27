fc1902a507b5be0b7c240229cb3c36b51497b36b
Rafael Faustini <contato@rafaelfaustini.com.br>
2020-12-26 23:05:10 -0300

    ToggleFullScreen mod

---
+++ ToggleFullScreenKey/ModEntry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace ToggleFullscreenKey
+{
+    public class ModEntry : Mod
+    {
+
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+        }
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            try
+            {
+                SButton botaoMenu = SButton.F11;
+                if (e.Button == botaoMenu)
+                {
+                    Game1.toggleFullscreen();
+                }
+            }
+            catch (Exception error)
+            {
+                this.Monitor.Log($"[ToggleFullscreenKey]: {error.Message}", LogLevel.Debug);
+                throw;
+            }
+
+
+        }
+    }
+}
